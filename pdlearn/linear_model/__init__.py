c51db17b34d2a9aa8e9fa51893716c184b745644
Rich Lewis <rl403@cam.ac.uk>
2015-12-07 01:46:21 +0000

    DEV: added LogisticRegression

---
+++ pdlearn/linear_model/__init__.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of pandas-learn
+# https://github.com/RichLewis42/pandas-learn
+#
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT
+# Copyright (c) 2015, Rich Lewis <rl403@cam.ac.uk>
+
+"""
+pdlearn.linear_model
+~~~~~~~~~~~~~~~~
+
+Package adapting scikit-learn's linear models.
+"""
+
+from .logistic import LogisticRegression
---
+++ pdlearn/linear_model/logistic.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of pandas-learn
+# https://github.com/RichLewis42/pandas-learn
+#
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT
+# Copyright (c) 2015, Rich Lewis <rl403@cam.ac.uk>
+
+"""
+pdlearn.linear_model.logistic
+~~~~~~~~~~~~~~~~
+
+Module adapting scikit-learn's logistic regresion models.
+"""
+
+from ..adaptor import model, classifier, feature_property
+
+import sklearn.linear_model
+
+# pylint: disable=R0901,C0111
+@model
+@classifier
+class LogisticRegression(sklearn.linear_model.LogisticRegression):
+
+    coef_ = feature_property('coef_')
